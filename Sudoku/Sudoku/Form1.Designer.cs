f603d733b3c657b5b89684e2502f9aa1b2c550eb
Dr-Left <39025643+Dr-Left@users.noreply.github.com>
2018-12-15 23:04:07 +0800

    Add files via upload

---
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(88, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1153, 734);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(69, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(968, 648);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(832, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(832, 505);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 770);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Sudoku V1.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] textBoxes= new TextBox[100];
+        public Form1()
+        {
+            InitializeComponent();
+            int Top = 10,Left=10;
+            for (int i = 0; i < 9; i++){
+                if (i % 3 == 0)
+                        Top += 10;
+                for (int j = 0; j < 9; j++)
+                {
+                    textBoxes[i * 9 + j] = new TextBox();
+                    textBoxes[i * 9 + j].Top = Top;
+                    if (j % 3 == 0)
+                        Left += 10;
+                    textBoxes[i * 9 + j].MaxLength = 1;
+                    textBoxes[i * 9 + j].Left = Left;
+                    textBoxes[i * 9 + j].Font = new Font("微软雅黑", 18, textBoxes[i * 9 + j].Font.Style | FontStyle.Bold);
+                    textBoxes[i * 9 + j].Height = textBoxes[i * 9 + j].Width = 40;
+                    textBoxes[i * 9 + j].TextAlign = HorizontalAlignment.Center;
+                    textBoxes[i * 9 + j].KeyPress += new KeyPressEventHandler(textBoxes_KeyPress);
+                    this.panel2.Controls.Add(textBoxes[i * 9 + j]);
+                    Left += 40;
+                }
+                Top += 40;
+                Left = 10;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = (sender as TextBox);
+            if(e.KeyChar == 37)
+            {
+                SelectNextControl(textBox, false, true, true, true);
+                return;
+            }
+            if (e.KeyChar == 13 || e.KeyChar == 32)
+            {
+                if(textBox.TextLength > 0 &&(!(textBox.Text[0]>='0' && textBox.Text[0]<='9')))
+                    textBox.Clear();
+            }
+            if (e.KeyChar == 8)
+            {
+                if(textBox.TextLength>0 &&!(textBox.Text[0]>='0' && textBox.Text[0]<='9'))
+                    textBox.Clear();
+                SelectNextControl(textBox, false, true, true, true);
+                return;
+            }
+            SelectNextControl(textBox, true, true, true, true);
+        }
+        public int[,] num = new int[10,10];
+        public bool sign = new bool();
+        public bool Check(int n, int key)
+        {
+            /* 判断n所在横列是否合法 */
+            for (int i = 0; i < 9; i++)
+            {
+                /* j为n竖坐标 */
+                int j = n / 9;
+                if (num[j,i] == key) return false;
+            }
+
+            /* 判断n所在竖列是否合法 */
+            for (int i = 0; i < 9; i++)
+            {
+                /* j为n横坐标 */
+                int j = n % 9;
+                if (num[i,j] == key) return false;
+            }
+
+            /* x为n所在的小九宫格左顶点竖坐标 */
+            int x = n / 9 / 3 * 3;
+
+            /* y为n所在的小九宫格左顶点横坐标 */
+            int y = n % 9 / 3 * 3;
+
+            /* 判断n所在的小九宫格是否合法 */
+            for (int i = x; i < x + 3; i++)
+            {
+                for (int j = y; j < y + 3; j++)
+                {
+                    if (num[i,j] == key) return false;
+                }
+            }
+
+            /* 全部合法，返回正确 */
+            return true;
+        }
+
+        /* 深搜构造数独 */
+        public int DFS(int n)
+        {
+            /* 所有的都符合，退出递归 */
+            if (n > 80)
+            {
+                sign = true;
+                return 0;
+            }
+            /* 当前位不为空时跳过 */
+            if (num[n / 9,n % 9] != 0)
+            {
+                DFS(n + 1);
+            }
+            else
+            {
+                /* 否则对当前位进行枚举测试 */
+                for (int i = 1; i <= 9; i++)
+                {
+                    /* 满足条件时填入数字 */
+                    if (Check(n, i) == true)
+                    {
+                        num[n / 9,n % 9] = i;
+                        /* 继续搜索 */
+                        DFS(n + 1);
+                        /* 返回时如果构造成功，则直接退出 */
+                        if (sign == true) return 0;
+                        /* 如果构造不成功，还原当前位 */
+                        num[n / 9,n % 9] = 0;
+                    }
+                }
+            }
+            return 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (textBoxes[i * 9 + j].TextLength == 0 || textBoxes[i * 9 + j].Text[0] == ' ')
+                        num[i, j] = 0;
+                    else
+                        num[i, j] = Convert.ToInt32(textBoxes[i * 9 + j].Text);
+                }
+            }
+            DFS(0);
+            if (sign == false)
+                MessageBox.Show("No Solution");
+            else
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    textBoxes[i * 9 + j].Text = Convert.ToString(num[i, j]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    textBoxes[i * 9 + j].Clear();
+                    num[i, j] = 0;
+                }
+            }
+            sign = false;
+        }
+    }
+}
